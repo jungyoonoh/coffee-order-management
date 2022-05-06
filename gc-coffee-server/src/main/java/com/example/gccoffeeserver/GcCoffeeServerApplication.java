f0d4763f1f612ab7645cd580e8a55ae0c9293174
Yoonoh <xkakak142@naver.com>
2022-05-07 00:40:23 +0900

    프로젝트 셋팅

---
+++ gc-coffee-server/src/main/java/com/example/gccoffeeserver/GcCoffeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gccoffeeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffeeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcCoffeeServerApplication.class, args);
+    }
+
+}
---
+++ gc-coffee-server/src/test/java/com/example/gccoffeeserver/GcCoffeeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gccoffeeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
