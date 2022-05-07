6fec8f1565744f95bdf61826f6d36e0fd9ea3635
Yoonoh <xkakak142@naver.com>
2022-05-07 18:55:42 +0900

    feat: order Service 구현

---
+++ gc-coffee-server/src/main/java/com/example/gccoffeeserver/service/DefaultOrderService.java
@@ -0,0 +1,37 @@
+package com.example.gccoffeeserver.service;
+
+import com.example.gccoffeeserver.model.Email;
+import com.example.gccoffeeserver.model.Order;
+import com.example.gccoffeeserver.model.OrderItem;
+import com.example.gccoffeeserver.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class DefaultOrderService implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public DefaultOrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems) {
+        return orderRepository.insert(new Order(email, address, postcode, orderItems));
+    }
+
+    @Override
+    @Transactional
+    public List<Order> getAllOrders() {
+        return orderRepository.findAllOrders();
+    }
+
+    @Override
+    public List<OrderItem> getOrderItemsByOrderId(UUID orderId) {
+        return orderRepository.findOrderItemsByOrderId(orderId);
+    }
+}
---
+++ gc-coffee-server/src/main/java/com/example/gccoffeeserver/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.example.gccoffeeserver.service;
+
+import com.example.gccoffeeserver.model.Email;
+import com.example.gccoffeeserver.model.Order;
+import com.example.gccoffeeserver.model.OrderItem;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderService {
+
+    Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems);
+
+    List<Order> getAllOrders();
+
+    List<OrderItem> getOrderItemsByOrderId(UUID orderId);
+}
